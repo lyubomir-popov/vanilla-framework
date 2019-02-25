ac842d3a3569899bc951b91fe2ac46aa40a139b4
Caleb Ellis <caleb.ellis@canonical.com>
2019-02-25 12:59:54 +0000

    Initial scaffolding

---
+++ src/app/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="p-strip">
+          <div className="row">
+            <h1>Copydoc generator</h1>
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
